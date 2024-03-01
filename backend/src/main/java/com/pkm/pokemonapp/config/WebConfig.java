7ed8c0db6c98a139bfd2e4ca9dc446f6e4e68d75
10652063 <mehmet.yazlak@thyssenkrupp.com>
2024-03-02 00:10:57 +0100

    add cors origin

--- backend/src/main/java/com/pkm/pokemonapp/config/WebConfig.java
+++ backend/src/main/java/com/pkm/pokemonapp/config/WebConfig.java
@@ -33,7 +33,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173","https://pkm-production.up.railway.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
